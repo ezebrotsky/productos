21f3e9b959a53aa07eea1e616ca5e89c4568f14e
Ezequiel Brotsky <ezequielbrotsky@gmail.com>
2019-09-20 14:02:11 -0300

    Print algunos precios

--- coto.py
+++ coto.py
@@ -8,6 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 import sys
 import re
+import time
 
 reload(sys)
 
@@ -25,12 +26,11 @@ categoriasCoto = soupCoto.find_all(id=regexCategoriasCoto)
 ## Función que recorre cada producto
 def recorreProducto(producto):
     for i in producto:
-        print(i.find(class_='span_productName').find(class_='descrip_full').get_text())
-    pass
-
-## Función que recorre cada precio
-def recorrePrecio(producto):
-    print(producto.find(class_='precioContado').get_text())
+        #time.sleep(10)
+        descripcion = i.find(class_='span_productName').find(class_='descrip_full').get_text()
+        precio = i.find_next(class_='atg_store_newPrice').get_text().replace('PRECIO CONTADO', '').replace('\n ', '').strip()
+        #print(i.find_next(class_='atg_store_productPrice').remove('style'))
+        print(descripcion + ' - ' + precio)
     pass
 
 ## Entro a las categorías
@@ -38,6 +38,8 @@ try:
     for cat in categoriasCoto:
         linksCoto = cat.find_all('a')
         for link in linksCoto:
+            print("Producto: " + link.get_text())
+
             href = link.get('href')
             rHref = requests.get('https://www.cotodigital3.com.ar'+href)
             soupPage = BeautifulSoup(rHref.text, "html.parser")
@@ -47,10 +49,11 @@ try:
             # Si tiene paginado
             if (ulPaginadoCoto):
                 li = ulPaginadoCoto.find_all('li')
-                numPag = 0
                 for pagina in li:
                     # Si no está seleccionada la página
                     if (pagina.find('a') and pagina.find('a').get('href')):
+                        print("Pagina: " + pagina.get_text().replace('\n', ''))
+
                         rPage = pagina.find('a').get('href')
                         
                         rHrefPage = requests.get('https://www.cotodigital3.com.ar'+rPage)
@@ -59,29 +62,16 @@ try:
                         ## Título del producto: 
                         productos = soupPageHref.find_all(class_='product_info_container')
                         recorreProducto(productos)
-                        ## Precio
-                        regexPrecios = re.compile('(\w*descrip_full_sku\w*)')
-                        ##precios = soupPageHref.find_all(class_=regexPrecios)
-                        ##recorrePrecio(precios)
-                        
                     # Si la página está seleccionada lo hago una vez
                     else:
                         ## Título del producto: 
                         productos = soupPage.find_all(class_='product_info_container')
                         recorreProducto(productos)
-                        ## Precio
-                        regexPrecios = re.compile('(\w*descrip_full_sku\w*)')
-                        ##precios = soupPage.find_all(class_=regexPrecios)
-                        ##recorrePrecio(precios)
             # Si no tiene paginado
             else:
                 ## Título del producto: 
                 productos = soupPage.find_all(class_='product_info_container')
                 recorreProducto(productos)
-                ## Precio
-                regexPrecios = re.compile('(\w*descrip_full_sku\w*)')
-                ##precios = soupPage.find_all(class_=regexPrecios)
-                ##recorrePrecio(precios)
 except Exception, e:
     print(e)
     
